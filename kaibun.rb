90c853181aec8090a571eb038e1f081722eabd65
Masafumi Yokoyama <myokoym@gmail.com>
2012-10-15 23:34:41 +0900

    Start kaibunkai

---
+++ kaibun.rb
@@ -0,0 +1,38 @@
+# encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+require 'MeCab'
+require 'mecab-modern'
+
+get "/" do
+  haml :index
+end
+
+post "/check" do
+  kana = reading(params['text'])
+  @message = "#{kana}: #{kaibun?(kana) ? '回文' : '違う'}"
+  haml :index
+end
+
+get "/check/:text" do
+  kaibun?(reading(text))
+end
+
+private
+def reading(text)
+  kana = ""
+  MeCab::Tagger.new.parseToNode(text).each do |node|
+    next if node.surface.empty?
+    kana << node.feature.split(/,/)[8]
+  end
+  hiragana(kana)
+end
+
+def hiragana(text)
+  text.tr("ァ-ヴ", "ぁ-ゔ").gsub(/[^ぁ-ゔ]/, '')
+end
+
+def kaibun?(text)
+  text == text.reverse
+end
---
+++ spec/kaibun_spec.rb
@@ -0,0 +1,40 @@
+# encoding: utf-8
+require 'rspec'
+require 'rack/test'
+
+MY_APP = Rack::Builder.parse_file('config.ru').first
+
+include Rack::Test::Methods
+def app
+  MY_APP
+end
+
+describe "kaibun-web" do
+  context "root" do
+    it "last response ok?" do
+      get '/'
+      last_response.ok? == true
+    end
+  end
+
+  context "check" do
+    it "竹藪焼けた" do
+      post '/check', {"text" => "竹藪焼けた"}
+      last_response.ok? == true
+      last_response.body.to_s.should =~ /たけやぶやけた: 回文/
+    end
+
+    it "竹藪、焼けた？" do
+      post '/check', {"text" => "竹藪、焼けた？"}
+      last_response.ok? == true
+      last_response.body.to_s.should =~ /たけやぶやけた: 回文/
+    end
+
+    it "竹藪焼けない" do
+      post '/check', {"text" => "竹藪焼けない"}
+      last_response.ok? == true
+      last_response.body.to_s.should =~ /たけやぶやけない: 違う/
+    end
+  end
+end
+
